137b639d8edadc904837e7dc5f9f7c01c39f79c7
Marcel Pfingstgräf <dev@pfingstgraef.eu>
2026-09-06 18:50:06 +0200

    style(web): place security settings in Figma settings shell

--- apps/web/src/app/app/settings/security/page.tsx
+++ apps/web/src/app/app/settings/security/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { SettingsShell } from "@/components/settings-shell";
 import { SecuritySettingsUi } from "./security-settings-ui";
 import { useSecuritySettings } from "./use-security-settings";
 
 export default function SecuritySettingsPage() {
   const controller = useSecuritySettings();
-  return <SecuritySettingsUi controller={controller} />;
+  return <SettingsShell><SecuritySettingsUi controller={controller} /></SettingsShell>;
 }
