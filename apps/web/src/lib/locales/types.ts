1a0409174ebf4fa2a6ef59a348c773559f6a54b8
Marcel Pfingstgräf <dev@pfingstgraef.eu>
2026-09-06 03:18:45 +0200

    feat: register shell translation keys

--- apps/web/src/lib/locales/types.ts
+++ apps/web/src/lib/locales/types.ts
@@ -1,4 +1,8 @@
 import type { en } from "./en";
 import type { enSearch } from "./en-search";
+import type { enShell } from "./en-shell";
 
-export type TranslationKey = keyof typeof en | keyof typeof enSearch;
+export type TranslationKey =
+  | keyof typeof en
+  | keyof typeof enSearch
+  | keyof typeof enShell;
